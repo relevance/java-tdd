34c0f3eb0d55b62e137b58883c2ea9c16ee4a8f7
Stuart Halloway <stu@thinkrelevance.com>
2008-07-09 20:35:57 -0400

    added groovy space station example

---
+++ examples/groovy/groovy/space/StationTest.groovy
@@ -0,0 +1,41 @@
+package groovy.space
+
+import static org.easymock.EasyMock.*;
+import examples.space.Station;
+import examples.space.Rocket
+
+class StationTest extends GroovyTestCase {
+  public void testRefuelShouldDecrementStationFuel() {
+    def station = new Station()
+    station.fuel = 1000
+    def rocket = createMock(Rocket)
+    station.refuel rocket, 50
+    assertEquals 950, station.fuel
+  }
+
+  public void testRefuelUsesTheHose() {
+    def station = new Station()
+    station.fuel = 1000
+    def rocket = createNiceMock(Rocket.class)
+    rocket.attachFuelHose()
+    rocket.detachFuelHose()
+    replay rocket 
+    station.refuel rocket, 50
+    verify rocket
+  }
+
+  public void testRefuelDoesThingsInTheCorrectOrder() {
+    def station = new Station();
+    station.fuel = 1000
+    def rocket = createStrictMock(Rocket.class)
+    rocket.dock()
+    rocket.attachFuelHose()
+    rocket.addFuel 50
+    rocket.detachFuelHose()
+    rocket.undock()
+    replay rocket
+    station.refuel rocket, 50
+    verify rocket
+  }
+
+}
